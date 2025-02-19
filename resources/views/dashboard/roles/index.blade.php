dbf712f67b5b3b2425701ef951b29ad12ff8d541
AndhikaPrasetya <andhikaprasetya542@gmail.com>
2025-02-19 17:08:16 +0700

    breadcrumb role

--- resources/views/dashboard/roles/index.blade.php
+++ resources/views/dashboard/roles/index.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.layout')
 @section('content')
 
-{{-- @include('layouts.breadcrumb') --}}
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>{{$title}}</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{url('/')}}">Dashboard</a></li>
+                    <li class="breadcrumb-item">{{$breadcrumb}}</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+  </section>
     <section class="content">
         <div class="container-fluid">
             <div class="row">
