d646b28eb47397b26c97bb20f9e28756b713bf29
AndhikaPrasetya <andhikaprasetya542@gmail.com>
2025-03-20 13:50:21 +0700

    development

--- resources/views/layouts/layoutSecond.blade.php
+++ resources/views/layouts/layoutSecond.blade.php
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('headTitle', 'Default Title')</title>
-    @if(app()->environment('production'))
+    @if(app()->environment('development'))
     <link rel="stylesheet" href="{{ secure_asset('build/assets/app.css') }}">
     <script src="{{ secure_asset('build/assets/app.js') }}" defer></script>
 @else
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 @endif
+
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="{{secure_asset('template/plugins/toastr/toastr.min.css')}}">
