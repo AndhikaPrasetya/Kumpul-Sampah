41685216fec64bfd59562e3d5ea08420e89fc182
AndhikaPrasetya <andhikaprasetya542@gmail.com>
2025-03-21 16:40:33 +0700

    test

--- resources/views/layouts/layoutSecond.blade.php
+++ resources/views/layouts/layoutSecond.blade.php
@@ -11,7 +11,6 @@
     <script src="{{ secure_asset('build/assets/app-CbEvcXly.js') }}" defer></script>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="{{secure_asset('template/plugins/toastr/toastr.min.css')}}">
 </head>
 
 <body class="bg-gray-50 font-sans">
@@ -28,9 +27,7 @@
        @yield('content')
     </div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-     <script src="{{secure_asset('template/plugins/toastr/toastr.min.js')}}"></script>
-
+  
     @yield('script')
 </body>
 
