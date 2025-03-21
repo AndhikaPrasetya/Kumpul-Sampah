02447eb0880a46afc3ba89578c4c59485687688a
AndhikaPrasetya <andhikaprasetya542@gmail.com>
2025-03-21 16:44:29 +0700

    test

--- resources/views/layouts/layoutSecond.blade.php
+++ resources/views/layouts/layoutSecond.blade.php
@@ -6,9 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('headTitle', 'Default Title')</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
+    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-CmeQu9Jz.css') }}">
+    <script src="{{ secure_asset('build/assets/app-CbEvcXly.js') }}" defer></script>
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="{{asset('template/plugins/toastr/toastr.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('template/plugins/toastr/toastr.min.css')}}">
 </head>
 
 <body class="bg-gray-50 font-sans">
@@ -25,8 +28,8 @@
        @yield('content')
     </div>
 
-    <script src="{{ asset('/template/plugins/jquery/jquery.min.js') }}"></script>
-     <script src="{{asset('template/plugins/toastr/toastr.min.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+     <script src="{{secure_asset('template/plugins/toastr/toastr.min.js')}}"></script>
 
     @yield('script')
 </body>
