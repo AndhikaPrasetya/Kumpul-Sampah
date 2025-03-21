407d35555b6d2be2dace3071d766fdd23a0b895d
AndhikaPrasetya <andhikaprasetya542@gmail.com>
2025-03-21 16:42:38 +0700

    test

--- resources/views/layouts/layoutSecond.blade.php
+++ resources/views/layouts/layoutSecond.blade.php
@@ -6,11 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('headTitle', 'Default Title')</title>
-    
-    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-CmeQu9Jz.css') }}">
-    <script src="{{ secure_asset('build/assets/app-CbEvcXly.js') }}" defer></script>
-
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="{{asset('template/plugins/toastr/toastr.min.css')}}">
 </head>
 
 <body class="bg-gray-50 font-sans">
@@ -27,7 +25,9 @@
        @yield('content')
     </div>
 
-  
+    <script src="{{ asset('/template/plugins/jquery/jquery.min.js') }}"></script>
+     <script src="{{asset('template/plugins/toastr/toastr.min.js')}}"></script>
+
     @yield('script')
 </body>
 
